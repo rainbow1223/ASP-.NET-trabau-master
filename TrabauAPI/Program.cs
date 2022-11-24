19815ec5437bc265de8123059fed82e5060151f6
StrongJack123 <greatwisdom1223@gmail.com>
2022-11-24 16:26:43 +0900

    complete the API

---
+++ TrabauAPI/Program.cs
@@ -0,0 +1,31 @@
+using Trabau.DataAccess.Repositories;
+using Trabau.DataAccess.Interfaces;
+using System.Net.Http;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+//}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
