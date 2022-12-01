b7030a26c8276e6976e306762f1923d7d8aaa7e7
StrongJack123 <greatwisdom1223@gmail.com>
2022-12-01 15:58:53 +0900

    add forgot password

---
+++ Web/forgotpassword.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="forgotpassword.aspx.cs" Inherits="forgotpassword" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Forgot / Reset Password</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/style-1.1.css">
+    <link rel="stylesheet" href="assets/css/responsive-1.1.css">
+    <script src="../assets/js/jquery-3.4.1.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <style>
+        html {
+            position: relative;
+            min-height: 100%;
+        }
+
+        body {
+            margin-bottom: 100px;
+            background: #f1f1f1;
+        }
+
+        #imgcaptcha {
+            margin-bottom: 15px;
+        }
+    </style>
+</head>
+<body style="background-image: url('assets/uploads/login-bg.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center; background-color: #0bbc56;">
+    <div class="off-canvas-container">
+        <main class="container">
+            <div class="air-card">
+                <div class="logo">
+                    <a href="#">
+                        <img src="assets/uploads/logo.png" alt="logo" /></a>
+                </div>
+                <h4>Reset your password</h4>
+                <form id="form1" runat="server">
+                    <div id="div_reset_request" runat="server">
+                        <div class="Validator-Container">
+                            <asp:TextBox ID="UserName" runat="server" CssClass="form-control" placeholder="Enter User Id or Email Address" autocomplete="Off"></asp:TextBox>
+                            <asp:RequiredFieldValidator runat="server" CssClass="text-danger custom-validator" ControlToValidate="UserName" SetFocusOnError="true"
+                                ErrorMessage="Enter User Id or Email Address" ValidationGroup="Trabau_ForgotPassword" Display="Dynamic" />
+                        </div>
+                        <div>
+                            <asp:Image ID="imgcaptcha" runat="server" />
+                        </div>
+                        <div class="Validator-Container">
+                            <asp:TextBox ID="txtCaptcha" runat="server" CssClass="form-control" placeholder="Enter captcha" autocomplete="Off"></asp:TextBox>
+                            <asp:RequiredFieldValidator runat="server" CssClass="text-danger custom-validator" ControlToValidate="txtCaptcha" SetFocusOnError="true"
+                                ErrorMessage="Enter captcha" ValidationGroup="Trabau_ForgotPassword" Display="Dynamic" />
+                        </div>
+                        <asp:Label ID="FailureText" runat="server" EnableViewState="False" ForeColor="Red"></asp:Label>
+                        <asp:Button ID="btnSendResetEmail" runat="server" Text="Request Reset Email" ValidationGroup="Trabau_ForgotPassword" CssClass="form-control"
+                            OnClick="btnSendResetEmail_Click" />
+                    </div>
+                    <div id="div_reset_request_message" runat="server" visible="false">
+                       <%-- <asp:Label ID="lblMessage" runat="server"></asp:Label>--%>
+                        <div id="div_message" runat="server"></div>
+                    </div>
+                </form>
+            </div>
+        </main>
+    </div>
+</body>
+</html>
---
+++ Web/forgotpassword.aspx.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TrabauClassLibrary;
+using TrabauClassLibrary.BLL;
+
+public partial class forgotpassword : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            LoadCaptcha();
+        }
+    }
+
+    public void LoadCaptcha()
+    {
+        Bitmap objBitmap = new Bitmap(130, 60);
+        Graphics objGraphics = Graphics.FromImage(objBitmap);
+        objGraphics.Clear(Color.White);
+        Random objRandom = new Random();
+        objGraphics.DrawLine(Pens.Black, objRandom.Next(0, 50), objRandom.Next(10, 30), objRandom.Next(0, 200), objRandom.Next(0, 50));
+        objGraphics.DrawRectangle(Pens.Blue, objRandom.Next(0, 20), objRandom.Next(0, 20), objRandom.Next(50, 80), objRandom.Next(0, 20));
+        objGraphics.DrawLine(Pens.Blue, objRandom.Next(0, 20), objRandom.Next(10, 50), objRandom.Next(100, 200), objRandom.Next(0, 80));
+        Brush objBrush =
+            default(Brush);
+        //create background style  
+        HatchStyle[] aHatchStyles = new HatchStyle[]
+        {
+                HatchStyle.BackwardDiagonal, HatchStyle.Cross, HatchStyle.DashedDownwardDiagonal, HatchStyle.DashedHorizontal, HatchStyle.DashedUpwardDiagonal, HatchStyle.DashedVertical,
+                    HatchStyle.DiagonalBrick, HatchStyle.DiagonalCross, HatchStyle.Divot, HatchStyle.DottedDiamond, HatchStyle.DottedGrid, HatchStyle.ForwardDiagonal, HatchStyle.Horizontal,
+                    HatchStyle.HorizontalBrick, HatchStyle.LargeCheckerBoard, HatchStyle.LargeConfetti, HatchStyle.LargeGrid, HatchStyle.LightDownwardDiagonal, HatchStyle.LightHorizontal
+        };
+        ////create rectangular area  
+        RectangleF oRectangleF = new RectangleF(0, 0, 300, 300);
+        objBrush = new HatchBrush(aHatchStyles[objRandom.Next(aHatchStyles.Length - 3)], Color.FromArgb((objRandom.Next(100, 255)), (objRandom.Next(100, 255)), (objRandom.Next(100, 255))), Color.White);
+        objGraphics.FillRectangle(objBrush, oRectangleF);
+        //Generate the image for captcha  
+        string captchaText = string.Format("{0:X}", objRandom.Next(1000000, 9999999));
+        //add the captcha value in session  
+        Session["Trabau_FP_CaptchaVerify"] = captchaText;
+        Font objFont = new Font("Courier New", 15, FontStyle.Bold);
+        //Draw the image for captcha  
+        objGraphics.DrawString(captchaText, objFont, Brushes.Black, 20, 20);
+        //  objBitmap.Save(HttpContext.Current.Response.OutputStream, ImageFormat.Gif);
+        byte[] _bytes;
+        using (MemoryStream ms = new MemoryStream())
+        {
+            objBitmap.Save(ms, ImageFormat.Bmp);
+            _bytes = ms.ToArray();
+        }
+        imgcaptcha.ImageUrl = "data:image;base64," + Convert.ToBase64String(_bytes);
+    }
+
+    protected void btnSendResetEmail_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            FailureText.Text = string.Empty;
+            string _captcha = txtCaptcha.Text;
+            string _actual_captcha = Session["Trabau_FP_CaptchaVerify"].ToString();
+            if (_captcha == _actual_captcha)
+            {
+                string _UserName = UserName.Text;
+                BLL_UserValidation obj = new BLL_UserValidation();
+                Tuple<List<dynamic>, string> data = obj.RequestSendEmail(_UserName, Request.UserHostAddress);
+                if (data != null)
+                {
+                    if (data.Item2 == "ok")
+                    {
+                        if (data.Item1 != null)
+                        {
+                            List<dynamic> res = data.Item1;
+                            if (res[0].Message == "1")
+                            {
+                                string EmailId = res[0].EmailId;
+                                string UserId = res[0].UserId.ToString();
+                                string UserId_Name = res[0].UserId_Name;
+                                string body = "";
+                                try
+                                {
+                                    string q1 = EncyptSalt.EncryptText(UserId + "^" + (DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToLongTimeString()), "Trabau_FP");
+                                    q1 = HttpUtility.UrlEncode(q1);
+
+                                    string template_url = "https://www.trabau.com/emailers/xxddcca/forgot-password.html";
+                                    WebRequest req = WebRequest.Create(template_url);
+                                    WebResponse w_res = req.GetResponse();
+                                    StreamReader sr = new StreamReader(w_res.GetResponseStream());
+                                    string html = sr.ReadToEnd();
+
+                                    html = html.Replace("@Name", UserId_Name);
+                                    html = html.Replace("@Link", "http://www.trabau.com/resetpassword.aspx?q1=" + q1);
+
+                                    body = html;
+
+                                    Emailer obj_email = new Emailer();
+                                    string _val = obj_email.SendEmail(EmailId, "", "", "Trabau Notification", "Your Trabau Account – Forgot your password?", body, null);
+
+
+                                }
+                                catch (Exception)
+                                {
+
+                                }
+                                div_reset_request_message.Visible = true;
+                                div_reset_request.Visible = false;
+                                div_message.InnerHtml = "An email has been sent your registered email address for reset password";
+                            }
+                            else
+                            {
+                                FailureText.Text = res[0].Message;
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                FailureText.Text = "Captcha not valid";
+            }
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+}
